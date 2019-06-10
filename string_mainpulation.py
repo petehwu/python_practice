141fb37cde169ffd96ff58f85819142206c941fe
Pete H Wu <pete.h.wu@gmail.com>
2019-06-10 12:56:40 -0700

    added some more string manipulations

--- string_mainpulation.py
+++ string_mainpulation.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
 
     s2 = st.translate(str.maketrans("", "", " "))
     print("removed all spaces:\n{}\n".format(s2))
+    """can also just use replace to remove a single character"""
+    s7 = st.replace(" ", "")
+    print("removed all spaces using replace:\n{}\n".format(s7))
 
     s3 = st.translate(str.maketrans("", "", "i"))
     print("remove all the i's:\n{}\n".format(s3))
@@ -21,6 +24,9 @@ if __name__ == "__main__":
 
     s5 = " ".join(st.translate(str.maketrans("ieo", "abc")).split())
     print("replace i with a, e with b, o with c and removed extra spaces:\n{}\n".format(s5))
+    
+    s8 = st.translate(str.maketrans("ieo", "abc", " "))
+    print("replace i with a, e with b, o with c and removed all spaces:\n{}\n".format(s8))
     
     s6 = st[::-1]
     print("reversed the string:\n{}\n".format(s6))
